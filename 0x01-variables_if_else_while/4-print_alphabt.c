c554c5e2b9edf7a843f1146da7ddc10658575ebb
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-05 05:11:26 +0000

    Hello

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main 
+ * 
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+    char c;
+    c = 'a';
+    while (c <= 'z')
+    {
+	putchar (c);
+	c++;
+	if (c=='e') || c == 'q')
+	c++;
+    }
+	{
+		putchar ('\n');
+	}
+	return (0);
+}

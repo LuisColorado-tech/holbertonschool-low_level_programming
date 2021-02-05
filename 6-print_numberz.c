3b6000f913e4112b1322a9ceb9da2ed8245e77ec
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-05 15:36:01 +0000

    hello

---
+++ 6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - E p
+ * description: print single number in base 10
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	putchar(a + '0');
+	putchar('\n');
+	return (0);
+}

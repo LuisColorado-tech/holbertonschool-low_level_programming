5cf63bfe78f179af2631ace398dc52d1312474f1
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-05 05:15:08 +0000

    Hello

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
- * main 
- * 
- * 
- * Return: 0
+ * main - Entry point
+ * description: program that prints alphabets in
+ * exept q and e
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c;
-    c = 'a';
-    while (c <= 'z')
-    {
-	putchar (c);
-	c++;
-	if (c=='e') || c == 'q')
-	c++;
-    }
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar (c);
+		c++;
+		if (c == 'e' || c == 'q')
+			c++;
+	}
 	{
 		putchar ('\n');
 	}

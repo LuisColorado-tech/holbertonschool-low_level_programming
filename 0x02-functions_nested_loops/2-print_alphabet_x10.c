e37ef538db2302773eb2af1223d5368e80b81296
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-09 15:42:27 +0000

    Hola

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,23 @@
 #include "holberton.h"
 /**
- * main - 
+ * print_alphabet_x10 - L
  * Description: use _putchar.c
- * Return: 0
+ * Return: 0 L
  */
 void print_alphabet_x10(void)
 {
-    int A;
-    int B = 0;
-
-    while (B < 10)
-    {
-        A = 'a';
-        while (A <= 'z')
-        {
-            _putchar(A);
-            A++;
-        }
-        _putchar('\n');
-        B++;
-    }
-    
+	int A;
+	int B = 0;
+	
+	while (B < 10)
+	{
+		A = 'a';
+		while (A <= 'z')
+		{
+			_putchar(A);
+			A++;
+		}
+			_putchar('\n');
+			B++;
+	}
 }

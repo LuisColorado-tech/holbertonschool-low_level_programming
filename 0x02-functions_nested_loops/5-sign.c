72d317e462ac06505ce57d4df5a0193d3dac6731
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-09 02:50:29 +0000

    Hello

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_singn - Print
+ * 
+ * œn : Arbi
+ * Return: 0 V
+ */
+ int print_sign(int n)
+ {
+     if (n > 0)
+     {
+         _putchar('+');
+         return (1);
+     }
+     else if (n < 0)
+     {
+         _putchar ('-');
+         return (-1);
+     }
+     else
+     {
+         _putchar('0');
+         return (0);
+     }
+ }

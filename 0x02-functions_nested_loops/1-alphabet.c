21948f92a14a8b01d9e4f3af134c245b3e5c917c
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-09 15:31:20 +0000

    Hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 /**
  * print_alphabet - prints the letters from a to z in lowercase
  * Description: use _putchar.c
- * Return: 0
+ * Return: 0 H
  */
 void print_alphabet(void)
 {
 	int letter = 'a';
-
 	while (letter <= 'z')
 	{
 		_putchar(letter);
 		letter++;
-	}
-	_putchar('\n');
+		}
+		_putchar('\n');
 }

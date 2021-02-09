0384831b50f22c479f6276c8be3d35f1207637e7
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-09 02:28:25 +0000

    hello

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * * abs
+ *  
+ * 
+ * Valor abs
+ * 
+ * Return: 0 Siempre
+ */
+int _abs(int n)
+{
+    if (n < 0)
+    {
+        return (-n);
+    }
+    else
+    {
+        return (n);
+    }
+}

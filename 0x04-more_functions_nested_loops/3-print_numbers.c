8a2b1ca261044c416e6d0b27bdfc273684dede82
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-11 21:40:31 +0000

    Hello

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_numbers - P N F 0 T 9
+ * Return: 0 success
+ */
+void print_numbers(void)
+{
+	int a;
+
+	a = 0;
+	while (a < 10)
+	{
+		_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n');
+}    

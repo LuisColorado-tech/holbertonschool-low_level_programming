f8835fc6320e6eb6242a6937b813576f0bf59b20
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-12 18:12:31 +0000

    Hello

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,30 @@
-gjsdlrfighflskdgmslkbjsb
+#include "holberton.h"
+
+/**
+ * print_diagonal - p a d l i t t.
+ * @n: t i
+ * description: c o use _putchar
+ */
+
+void print_diagonal(int n)
+{
+	int tab, row;
+
+	row = 0;
+
+	while (n > 0)
+	{
+		tab = row;
+		while (tab > 0)
+		{
+			_putchar(' ');
+			tab--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		row++;
+		n--;
+	}
+	if (row < 1)
+		_putchar('\n');
+}

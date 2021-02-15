98e83df9ac14ee9a011678718935d5765ebd9558
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-15 22:40:24 +0000

    Holbie

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - T a P to an int as a P.
+ * @n: type int pointer
+ * description: Update the value to 98
+ * Return: 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

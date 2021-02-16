b661ec45ad880e4713cd339a3923fd465ecaed82
LuisColorado-tech <luiscolorado@outlook.com>
2021-02-16 20:48:54 +0000

    Hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - R the l of a s.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+		return (c);
+
+}

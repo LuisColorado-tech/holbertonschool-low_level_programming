103b643728ed3517d8cec42c6470def5875bc85a
LuisColorado-Tech <luiscolorado@outlook.com>
2021-03-02 07:02:29 +0000

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - p n f by a n l
+ * @argc: type int argument
+ * @argv: type char argument of string.
+ * Return: none
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

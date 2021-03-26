3cac05b5e85f4565882d753b71e72a8e5b322106
LuisColorado-Tech <luiscolorado@outlook.com>
2021-03-26 17:47:49 +0000

    Complete the project

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,52 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - adds positive numbers
- * @argv: array 
+ * main - print the min number of coins to make change for an amount of money
  * @argc: argument count
- * Return: 0 or 1
+ * @argv: argument vector, array of strings
+ * Return: 1 if error, 0 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int total, devuelta;
+	int change, input;
+	unsigned int x;
+	char *p;
+	int coins[] = {25, 10, 5, 2};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-    devuelta = atoi(argv[1]);
 
-if (devuelta < 0)
-    printf("0\n");
-    return (1);
-}
-    for (total = 0; devuelta > 0; total++)
+	input = strtol(argv[1], &p, 10);
+	change = 0;
+
+	if (!*p)
 	{
-		if (devuelta - 25 >= 0)
-			devuelta = devuelta - 25;
-		else if (devuelta - 10 >= 0)
-			devuelta = devuelta - 10;
-		else if (devuelta - 5 >= 0)
-			devuelta = devuelta - 5;
-		else if (devuelta - 2 >= 0)
-			devuelta = devuelta - 2;
-		else if (devuelta - 1 >= 0)
-			devuelta = devuelta - 1;
-    
+		while (input > 1)
+		{
+			for (x = 0; x < sizeof(coins[x]); x++)
+			{
+				if (input >= coins[x])
+				{
+					change += input / coins[x];
+					input %= coins[x];
+				}
+			}
+		}
+		if (input == 1)
+			change++;
 	}
-	printf("%d\n", total);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", change);
 	return (0);
-    
 }

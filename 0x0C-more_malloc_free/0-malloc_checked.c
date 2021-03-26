862350d71e766d84d4638e52c7d234b80d6266ed
LuisColorado-Tech <luiscolorado@outlook.com>
2021-03-26 18:49:15 +0000

    More malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - create array of char.
+ * @b: type unsigned int.
+ * Return: Pointer of allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = NULL;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
